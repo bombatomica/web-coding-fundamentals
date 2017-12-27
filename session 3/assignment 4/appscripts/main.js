856581a20787bb6688669ecbbab651d4aba0409c
bombatomica <33621799+bombatomica@users.noreply.github.com>
2017-12-27 21:52:11 +0800

    assignment 4 - work in progress

--- session 3/assignment 4/appscripts/main.js
+++ session 3/assignment 4/appscripts/main.js
@@ -9,8 +9,32 @@ require(
         /* assign3: font family for article in JavaScript */
         document.getElementById("articleID").style.fontFamily = "Comic Sans MS, cursive, sans-serif"; //"Impact,Charcoal,sans-serif"; 
 
-        //--------------------------------------------------------------
+        var mySliderHue = document.getElementById("mySlider1");
+        var mySliderSaturation = document.getElementById("mySlider2");
+        var mySliderLightness = document.getElementById("mySlider3");
+        // I get my sliders from the DOM usong the id's I gave them in the html document
+        var articleBox = document.getElementById("articleID");
 
  
-        }
-);
+        //--------------------------------------------------------------
+
+ 		var hslString = function(h, s, l) {
+ 		/*I defined a function named it "hslString and gave it 3 arguments" */
+
+ 			var constructHslString = "hsl("+ h + "," + s + "%," + l + "%)";
+ 			/*I used the arguments in function to construct the hsl string*/
+ 			return constructHslString;
+ 			/*My function returns the string I created*/
+ 		}
+ 		console.log("hslString is" + hslString);
+ 		/*I print the string that the function returns out to the console */
+		articleBox.style.backgroundColor = hslString(0, 50%, 100%);
+
+ 		mySlider1.addEventListener("input", function (ev){
+ 			articleBox.style.backgroundColor = hslString;
+ 		})
+ 		
+
+
+ 	 		
+});
