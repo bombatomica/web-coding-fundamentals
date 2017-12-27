e3f8318ca3c18655fd0964ec0a8ddcf72148cb90
bombatomica <33621799+bombatomica@users.noreply.github.com>
2017-12-27 21:48:24 +0800

    assignment 4 - not started

---
+++ session 3/assignment 4/appscripts/main.js
@@ -0,0 +1,16 @@
+require(
+	[],
+
+	function () {
+        console.log("yo");
+        /* assign3: center header text using javascript */
+        document.getElementById("headerID").style.textAlign = "center";
+
+        /* assign3: font family for article in JavaScript */
+        document.getElementById("articleID").style.fontFamily = "Comic Sans MS, cursive, sans-serif"; //"Impact,Charcoal,sans-serif"; 
+
+        //--------------------------------------------------------------
+
+ 
+        }
+);
