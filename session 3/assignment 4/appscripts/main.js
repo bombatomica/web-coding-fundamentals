6de34a9640bdf5a79e93b3449336b6a652fdc30b
bombatomica <33621799+bombatomica@users.noreply.github.com>
2018-01-02 19:39:36 +0800

    work in progress

--- session 3/assignment 4/appscripts/main.js
+++ session 3/assignment 4/appscripts/main.js
@@ -9,32 +9,50 @@ require(
         /* assign3: font family for article in JavaScript */
         document.getElementById("articleID").style.fontFamily = "Comic Sans MS, cursive, sans-serif"; //"Impact,Charcoal,sans-serif"; 
 
+        //Get your sliders from the DOM using the id's you gave them in the html document
+        var mySliderOpacity = document.getElementById("mySlider");
         var mySliderHue = document.getElementById("mySlider1");
         var mySliderSaturation = document.getElementById("mySlider2");
         var mySliderLightness = document.getElementById("mySlider3");
-        // I get my sliders from the DOM usong the id's I gave them in the html document
+        // get your article id from the dom
         var articleBox = document.getElementById("articleID");
 
  
         //--------------------------------------------------------------
-
- 		var hslString = function(h, s, l) {
- 		/*I defined a function named it "hslString and gave it 3 arguments" */
-
- 			var constructHslString = "hsl("+ h + "," + s + "%," + l + "%)";
- 			/*I used the arguments in function to construct the hsl string*/
- 			return constructHslString;
- 			/*My function returns the string I created*/
+        /*Define a function named it "hslString and gave it 3 arguments" */
+ 	var hslString = function(h, s, l) {
+                /*Use the arguments in function to construct the hsl string*/
+		var constructHslString = "hsl("+ h + "," + s + "%," + l + "%)";
+                /*Your function returns the string I created*/
+		return constructHslString;
  		}
+                /*Print the string that the function returns out to the console */
  		console.log("hslString is" + hslString);
- 		/*I print the string that the function returns out to the console */
-		articleBox.style.backgroundColor = hslString(0, 50%, 100%);
 
- 		mySlider1.addEventListener("input", function (ev){
- 			articleBox.style.backgroundColor = hslString;
- 		})
+ 	// Let the user adjust the opacity through slider	
+	mySlider.addEventListener("input", function(ev){
+                articleBox.style.opacity = mySlider.value;
+                });
+        //Let the user adjust the background color
+      
+        
+        //give articleBox an eventlistener to Make it completely opaque while the mouse is down, and set it back to the
+        //opacity slider setting when the mouse is up.
+        articleBox.addEventListener("mousedown", mouseDown);
+        articleBox.addEventListener("mouseup", mouseUp);
+
+        function mouseDown() {
+                articleBox.style.opacity = 0;
+        }
+
+        function mouseUp(){
+                articleBox.style.opacity = 100;
+        }
+
+ 		
  		
 
 
  	 		
-});
+                }
+        );
